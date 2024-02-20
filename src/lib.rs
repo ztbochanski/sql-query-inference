7f89607348234fe8adc0a5ac428c30884facdffc
Zach Bochanski <ztbochanski@gmail.com>
2024-02-20 06:17:44 -0800

    add inference logic
    - read in file
    - parse cli args
    - deserialize read into rust struct
    - now that data is captured in rust data structures, use the query lib we made to fetch the a map
    - compare table columns and names and join into a super set
    - give options for output to csv or json
    - sort alpha

---
+++ src/lib.rs
@@ -0,0 +1,69 @@
+mod query;
+mod cli;
+mod csv_ingester;
+mod json_creator;
+
+use std::error::Error;
+use std::collections::HashMap;
+use structopt::StructOpt;
+
+use cli::Cli;
+use csv_ingester::{read_csv_file, write_to_csv};
+use json_creator::write_to_json;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let args = Cli::from_args();
+    let mut reader = read_csv_file(&args.input)?;
+    
+    let mut tables_columns_map: Vec<Vec<query::TableMetadata>> = Vec::new();
+    let mut counter = 0;
+    for result in reader.deserialize::<query::Query>() {
+        let query: query::Query = result?;
+        let tables_columns: Vec<query::TableMetadata> = query.map_tables_columns();
+        tables_columns_map.push(tables_columns);
+        counter += 1;
+    }
+
+    println!("Number of Queries Processed: {}", counter);
+
+
+    let mut flattened_map: Vec<query::TableMetadata> = Vec::new();
+    for inner_vec in tables_columns_map {
+        flattened_map.extend(inner_vec);
+    }
+    let mut table_columns_map: HashMap<String, Vec<String>> = HashMap::new();
+
+    for metadata in &flattened_map {
+        let columns = table_columns_map.entry(metadata.table_name.clone()).or_insert(Vec::new());
+        columns.extend(metadata.columns.iter().cloned());
+        columns.sort();
+        columns.dedup();
+    }
+
+    let mut modified_flattened_map: Vec<query::TableMetadata> = Vec::new();
+    for (table_name, columns) in table_columns_map {
+        modified_flattened_map.push(query::TableMetadata {
+            table_name,
+            columns,
+        });
+    }
+
+    modified_flattened_map.sort_by_key(|metadata| metadata.table_name.clone());
+
+
+    match args.format.as_str() {
+        "json" => {
+            write_to_json("output.json", modified_flattened_map)?;
+            println!("Data has been written to output.json");
+        },
+        "csv" => {
+            write_to_csv("output.csv", modified_flattened_map)?;
+            println!("Data has been written to output.csv");
+        },
+        _ => {
+            eprintln!("Unsupported output format: {}", args.format);
+            return Ok(());
+        }
+    }
+    Ok(())
+}
