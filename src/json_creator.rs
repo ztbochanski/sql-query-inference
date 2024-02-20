e132214ea66ec1352d32dde607816a7e3f446acd
Zach Bochanski <ztbochanski@gmail.com>
2024-02-20 14:00:33 -0800

    reactore to use generalized types
    allows flexibility of usage

--- src/json_creator.rs
+++ src/json_creator.rs
@@ -1,15 +1,18 @@
-use crate::query::TableMetadata;
-
+use serde::Serialize;
 use serde_json;
 use std::error::Error;
 
-fn create_json_string(data: &[TableMetadata]) -> Result<String, Box<dyn Error>> {
+
+pub fn create_json_string<T: Serialize>(data: &[T]) -> Result<String, Box<dyn Error>> {
     let json_string = serde_json::to_string_pretty(data)?;
     Ok(json_string)
 }
 
-pub fn write_to_json(filename: &str, data: Vec<TableMetadata>) -> Result<(), Box<dyn Error>> {
-    let json_string = create_json_string(&data)?;
+pub fn write_to_json<T: Serialize>(
+    filename: &str,
+    data: &[T]
+) -> Result<(), Box<dyn Error>> {
+    let json_string = create_json_string(data)?;
     std::fs::write(filename, json_string)?;
     Ok(())
 }
